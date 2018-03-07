83970ed613d7234a53c3a20cb0e37ad4a90e93fd
nliu3177 <35586783+nliu3177@users.noreply.github.com>
2018-03-07 13:44:43 -0600

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,5 +1,48 @@
 package pagerism;
 
 public class Book {
-
+  /*
+	 * table: book
+	 * 		id:isbn
+	  		name 
+	  		author 
+	  		price double
+	  		description
+	 */
+	private String id;
+	private String name;
+	private String author;
+	private double price;
+	private String description;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }
