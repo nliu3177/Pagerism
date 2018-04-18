e76580a62ce991c53761b6ea5fa4d91f50d99a34
Epiprometheus <35313202+Epiprometheus@users.noreply.github.com>
2018-04-17 23:29:42 -0500

    Add files via upload

--- Login.java
+++ Login.java
@@ -2,12 +2,7 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
+import java.sql.*;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -23,6 +18,26 @@ public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     
 
+	// prints the header of an html page
+    void pageHead(PrintWriter out)
+    {
+    	out.println("<html>");
+		out.println("<head></head>");
+		out.println("<body>");
+		out.println("<meta charset=\"ISO-8859-1\">");
+		out.println("<title>Class Entry</title>");
+		out.println("</head>");
+		out.println("<body>");
+    }
+    
+  //prints the footer of an html page
+    void pageEnd(PrintWriter out)
+    {
+    	out.println("</form>");
+		out.println("</body>");
+		out.println("</html>");
+    }
+    
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -35,40 +50,51 @@ public class Login extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter out = response.getWriter();
-	
-		PagerismSqlDriver psql = new PagerismSqlDriver();
 		
-		out.println("<!DOCTYPE html>");
-        out.println("<html><head>");
-        out.println("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>");
-        out.println("<title>Pagerism Login</title>");
-        out.println("</head><body>");
-        
-        if(request.getParameter("login") != null){
-        	out.println("Login<br>");
-        	out.println("User ID: " + psql.logIn(request.getParameter("username"), request.getParameter("password")));
-        }
-        else if(request.getParameter("register") != null){
-        	out.println("Register<br>");
-        	psql.registerUser(request.getParameter("username"), request.getParameter("password"));
-        	out.println("User: " + request.getParameter("username") + " Registered!");
-        }
-        
-        out.println("</body>"
-        		+ 	"</html>");
-		out.close();
-		
-		//RequestDispatcher rd = request.getRequestDispatcher("store");
-		//rd.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		int id = -1;
+		PrintWriter out = response.getWriter();
+		PagerismSqlDriver hope = new PagerismSqlDriver();
+		
+		if(request.getParameter("log") == "forgot")
+		{
+			pageHead(out);
+			out.println("<form method=\"get\" action=\"Login\">");
+			if(hope.validUser(request.getParameter("username")))
+			{
+				out.println("<form\"post\" action=\"Login\">");
+				out.println("New Password: <input type=\"text\" name=\"password\"/><br/>");
+				out.println("<input type=\"hidden\" name=\"log\" value = \"create\"/><br/>");
+				out.println("name=\"username\" value = \"" + request.getParameter("username") + "\"/><br/>");
+				out.println("<input type=\"submit\" value=\"Submit\"/>");
+				out.println("</form>");
+			}
+			else
+			{
+				out.println("Invalid username");
+			}
+			pageEnd(out);
+			
+			
+		}
+		
+		else if(request.getParameter("log") == "create")
+		{
+			id = hope.registerUser(request.getParameter("username"), request.getParameter("password"));
+		}
+		
+		else
+		{
+			id = hope.logIn(request.getParameter("username"), request.getParameter("password"));
+		}
+		
+		
+		response.sendRedirect("<Shop>?userid="+ id);
 	}
 
 }
